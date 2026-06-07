7f9939e6c0d4ffea3b22b64ae6826664985396d4
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-07 17:36:19 -0300

    feat(squeaky-clean): solve exercism exercise

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,7 +1,37 @@
+using System.Text;
+
 public static class Identifier
 {
+    private static string GreekAlphabet = "αβγδεζηθικλμνξοπρστυφχψω";
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        if(string.IsNullOrEmpty(identifier)) return string.Empty;
+
+        var buffer = new StringBuilder();
+
+        for( int i = 0 ; i < identifier.Length ; i++ )
+        {
+            char c = identifier[i];
+
+            if(char.IsLetter(c) && !GreekAlphabet.Contains(c))
+            {
+                buffer.Append(c); continue;
+            }
+            else if(c == ' ')
+            {
+                buffer.Append('_'); 
+            }
+            else if(c == '\0')
+            {
+                buffer.Append("CTRL");
+            }
+            else if(c == '-' && i < identifier.Length - 1)
+            {
+                c = identifier[++i];
+                buffer.Append(char.ToUpperInvariant(c)); 
+            }
+        }
+
+        return buffer.ToString();
     }
 }
