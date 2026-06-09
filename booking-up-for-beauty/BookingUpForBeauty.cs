467a546d6b16931b87f87ca36b707171e0b0b9a5
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-09 15:28:06 -0300

    feature(booking-up-for-beauty): solve exercise

--- booking-up-for-beauty/BookingUpForBeauty.cs
+++ booking-up-for-beauty/BookingUpForBeauty.cs
@@ -1,27 +1,19 @@
 static class Appointment
 {
-    public static DateTime Schedule(string appointmentDateDescription)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
-    }
+    private static DateTime Anniversary =>
+        new(DateTime.Today.Year, 9, 15);
 
-    public static bool HasPassed(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasPassed() method");
-    }
+    public static DateTime Schedule(string appointmentDateDescription) =>
+        DateTime.Parse(appointmentDateDescription);
 
-    public static bool IsAfternoonAppointment(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.IsAfternoonAppointment() method");
-    }
+    public static bool HasPassed(DateTime appointmentDate) =>
+        appointmentDate < DateTime.Now;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
+    public static bool IsAfternoonAppointment(DateTime appointmentDate) =>
+        appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
 
-    public static DateTime AnniversaryDate()
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.AnniversaryDate() method");
-    }
+    public static string Description(DateTime appointmentDate) =>
+        $"You have an appointment on {appointmentDate}.";
+
+    public static DateTime AnniversaryDate() => Anniversary;
 }
